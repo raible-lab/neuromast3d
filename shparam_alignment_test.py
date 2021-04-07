6b0296dba67f058a040ebeb2fdc25da29fe1fce1
Madeleine <63812903+memoryleine@users.noreply.github.com>
2021-04-06 18:59:35 -0700

    Remove functions to fix bg label issue

--- shparam_alignment_test.py
+++ shparam_alignment_test.py
@@ -2,38 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import os
+
 import numpy as np
 from scipy.ndimage import center_of_mass
 from skimage.measure import regionprops
 from skimage.morphology import ball, binary_closing
+
 from aicsimageio import AICSImage
 from aicsshparam import shtools, shparam
-
-
-def get_largest_cc(image):
-    largest_cc = np.argmax(np.bincount(image.flat))
-    return largest_cc
-
-
-def switch_label_values(label_image, first, second):
-    last_label = label_image.max()
-    label_image = np.where(label_image == first, last_label + 1, label_image)
-    label_image = np.where(label_image == second, first, label_image)
-    label_image = np.where(label_image == last_label + 1, second, label_image)
-    return label_image
-
 
 project_dir = '/home/maddy/projects/claudin_gfp_5dpf_airy_live/'
 test_nm = '20200617_1-O1'
 test_nm_dir = f'{project_dir}/single_cell_masks/{test_nm}'
 
 whole_seg_reader = AICSImage(
-        f'{project_dir}/label_images/{test_nm}_rawlabels.tiff'
+        f'{project_dir}/label_images_fixed_bg/{test_nm}_rawlabels.tiff'
         )
 whole_seg_img = whole_seg_reader.get_image_data('ZYX', S=0, T=0, C=0)
-
-lcc = get_largest_cc(whole_seg_img)
-whole_seg_img = switch_label_values(whole_seg_img, 0, lcc)
 
 whole_nm = whole_seg_img[whole_seg_img > 0].astype(np.uint8)
 whole_nm = binary_closing(whole_nm, ball(5))
