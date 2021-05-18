e3cb74ba633b86ad5511289b44368395a354b79b
Madeleine <63812903+memoryleine@users.noreply.github.com>
2021-05-17 22:50:54 -0700

    Remove magicgui, add saving raw/edited labels

--- nucleus_segmentation.py
+++ nucleus_segmentation.py
@@ -15,8 +15,6 @@ import sys
 from aicsimageio import AICSImage
 from aicsimageio.writers import ome_tiff_writer
 import napari
-from napari.types import LabelsData, ImageData
-from magicgui import magicgui
 import numpy as np
 from scipy import ndimage as ndi
 from skimage import morphology, filters
@@ -24,12 +22,11 @@ from skimage.feature import peak_local_max
 from skimage.segmentation import watershed
 
 
-@magicgui(call_button='Run DT watershed')
 def dt_watershed(
-        image: ImageData,
+        image: np.array,
         sigma: int,
         min_distance: int
-) -> LabelsData:
+) -> np.array:
     distance = ndi.distance_transform_edt(image)
     distance_smoothed = filters.gaussian(
             distance,
@@ -56,18 +53,18 @@ parser = argparse.ArgumentParser(
 parser.add_argument('raw_dir', help='directory containing raw input images')
 parser.add_argument('mask_dir', help='directory containing nuclear masks')
 parser.add_argument('output_dir', help='directory to save output labels')
-# parser.add_argument('sigma', help='sigma to use for Gaussian blur of \
-#        distance transform')
-# parser.add_argument('min_distance', help='min_distance parameter for \
-#        peak_local_max function')
+parser.add_argument('sigma', help='sigma to use for Gaussian blur of \
+        distance transform')
+parser.add_argument('min_distance', help='min_distance parameter for \
+        peak_local_max function')
 
 args = parser.parse_args()
 
 raw_dir = args.raw_dir
 mask_dir = args.mask_dir
 output_dir = args.output_dir
-# sigma = args.sigma
-# min_distance = args.min_distance
+sigma = args.sigma
+min_distance = args.min_distance
 
 # Check raw and mask directories exist
 if not os.path.isdir(raw_dir):
@@ -109,11 +106,31 @@ for img_id in list_of_img_ids:
     # Use eroded membrane mask to split touching nuclei
     nuclei_split = np.where(mem_eroded, 0, nuc_mask)
 
-    # Inspect the 'splitting' results in the napari viewer
-    viewer = napari.view_image(nuclei, colormap='gray', blending='additive')
+    # Apply dt watershed
+    ws_results = dt_watershed(
+            nuclei_split,
+            sigma=sigma,
+            min_distance=min_distance
+    )
+
+    # Save raw (unedited) labels
+    raw_save_path = Path(f'{output_dir}/raw_nuc_labels/{img_id}_rawlabels.tiff')
+    raw_save_path.mkdir(parents=True, exist_ok=True)
+    writer = ome_tiff_writer.OmeTiffWriter(raw_save_path)
+    writer.save(ws_results, dimension_order='ZYX') 
+
+    # Inspect the results in the napari viewer
+    viewer = napari.Viewer()
+    viewer.add_image(nuclei, colormap='gray', blending='additive')
+    viewer.add_image(membranes, colormap='cyan', blending='additive')
     viewer.add_image(nuc_mask, colormap='magenta', blending='additive')
     viewer.add_image(nuclei_split, colormap='green', blending='additive')
-
-    # Add dt watershed widget
-    viewer.window.add_dock_widget(dt_watershed)
+    label_layer = viewer.add_labels(ws_results)
     napari.run()
+
+    # Save edited labels from napari
+    edited_labels = label_layer.data
+    edited_save_path = Path(f'{output_dir}/raw_nuc_labels/{img_id}_editedlabels.tiff')
+    writer = ome_tiff_writer.OmeTiffWriter(edited_save_path)
+    writer.save(edited_labels, dimension_order='ZYX')
+
